c28101b8a7d9a4b4c46234aca91e2f192b13e8df
zhennann <zhen.nann@icloud.com>
2023-10-19 09:35:24 +0800

    feat: first commit

---
+++ dist/index.js
@@ -0,0 +1,83 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ cascadeExtend)
+/* harmony export */ });
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+function cascadeExtend(_ref) {
+  var scope = _ref.scope,
+    source = _ref.source,
+    name = _ref.name;
+  if (!source || !name) return null;
+  var keys = Object.keys(source).filter(function (key) {
+    return key === name || key.indexOf(name + '_') === 0;
+  });
+  if (keys.length === 0) return null;
+  if (keys.length === 1 && keys[0] === name) return source[keys[0]];
+  var result = {};
+  var nameLength = name.length;
+  var _iterator = _createForOfIteratorHelper(keys),
+    _step;
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var key = _step.value;
+      if (key === name) {
+        extend(true, result, source[key]);
+      } else {
+        var parts = key.substring(nameLength + 1).split('_');
+        var test = parts.every(function (part) {
+          return !!scope[part];
+        });
+        if (test) {
+          extend(true, result, source[key]);
+        }
+      }
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+  return result;
+}
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
---
+++ src/main.js
@@ -0,0 +1,22 @@
+export default function cascadeExtend({ scope, source, name }) {
+  if (!source || !name) return null;
+  const keys = Object.keys(source).filter(key => {
+    return key === name || key.indexOf(name + '_') === 0
+  });
+  if (keys.length === 0) return null;
+  if (keys.length === 1 && keys[0] === name) return source[keys[0]];
+  const result = {};
+  const nameLength = name.length;
+  for (const key of keys) {
+    if (key === name) {
+      extend(true, result, source[key]);
+    } else {
+      const parts = key.substring(nameLength + 1).split('_');
+      const test = parts.every(part => !!scope[part]);
+      if (test) {
+        extend(true, result, source[key]);
+      }
+    }
+  }
+  return result;
+}
