fb0a018326724ea68cfd96509f00fc94b3afe373
zhennann <zhen.nann@icloud.com>
2023-12-11 20:47:52 +0800

    feat: keys sort

--- src/main.mjs
+++ src/main.mjs
@@ -2,16 +2,17 @@ import extend from '@zhennann/extend';
 
 export default function cascadeExtend({ scope, source, name }) {
   if (!source || !name) return null;
+  // filter
   const keys = Object.keys(source).filter(key => {
     return key === name || key.indexOf(name + '_') === 0
   });
   if (keys.length === 0) return null;
   if (keys.length === 1 && keys[0] === name) return source[keys[0]];
-  keys.sort((a,b)=>{
-    const a2=a.split('_').length;
-    const b2=b.split('_').length;
-    return a2-b2;
+  // sort
+  keys.sort((a, b) => {
+    return a.split('_').length - b.split('_').length;
   })
+  // extend
   const result = {};
   const nameLength = name.length;
   for (const key of keys) {
