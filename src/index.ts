39ca2847ebd4aeb085b030eb0b3cd8ebee82c9ac
zhennann <zhen.nann@icloud.com>
2024-04-24 10:56:31 +0800

    feat: cascadeExtendKeys

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
 import { extend } from '@cabloy/extend';
 
+export function cascadeExtendKeys(
+  scope: object,
+  source: object | undefined,
+  name?: string,
+  sep: string = '_',
+): string[] | undefined {
+  if (!source) return undefined;
+  if (name === undefined) name = '';
+  // filter
+  let keys = Object.keys(source).filter(key => {
+    return key === name || key.indexOf(name + sep) === 0;
+  });
+  if (keys.length === 0) return undefined;
+  if (keys.length === 1 && keys[0] === name) return keys;
+  // sort
+  keys.sort((a, b) => {
+    return a.split('_').length - b.split('_').length;
+  });
+  // filter
+  const nameLength = name.length;
+  keys = keys.filter(key => {
+    if (key === name) return true;
+    const parts = key.substring(nameLength + 1).split(sep);
+    return parts.every(part => !!scope[part]);
+  });
+  // ok
+  return keys.length === 0 ? undefined : keys;
+}
+
 export function cascadeExtend({ scope, source, name }) {
-  if (!source || !name) return null;
+  if (!source) return null;
   // filter
   const keys = Object.keys(source).filter(key => {
     return key === name || key.indexOf(name + '_') === 0;
