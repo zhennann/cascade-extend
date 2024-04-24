60c15cee5c5d7d79106f338e46b5fe607a002cec
zhennann <zhen.nann@icloud.com>
2024-04-24 10:38:13 +0800

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import {extend} from '@cabloy/extend';
+import { extend } from '@cabloy/extend';
 
 export function cascadeExtend({ scope, source, name }) {
   if (!source || !name) return null;
   // filter
   const keys = Object.keys(source).filter(key => {
-    return key === name || key.indexOf(name + '_') === 0
+    return key === name || key.indexOf(name + '_') === 0;
   });
   if (keys.length === 0) return null;
   if (keys.length === 1 && keys[0] === name) return source[keys[0]];
   // sort
   keys.sort((a, b) => {
     return a.split('_').length - b.split('_').length;
-  })
+  });
   // extend
   const result = {};
   const nameLength = name.length;
--- test/test.mjs
+++ test/test.mjs
@@ -1,33 +1,33 @@
 import { cascadeExtend } from '../dist/index.js';
 
 const scope = {
-    group: true,
-    mobile: true,
-    small: true,
-    view: true,
-}
+  group: true,
+  mobile: true,
+  small: true,
+  view: true,
+};
 const source = {
-    ebParams_mobile: {
-        size: 'small',
-        scene: 'mobile',
-        group: true,
-    },
-    ebParams: {
-        size: 'large',
-        scene: 'pc',
-        name: 'yang',
-    },
-    ebParams_pc: {
-        size: 'large',
-        scene: 'pc',
-        name: 'kevin',
-        group: true,
-    },
-}
+  ebParams_mobile: {
+    size: 'small',
+    scene: 'mobile',
+    group: true,
+  },
+  ebParams: {
+    size: 'large',
+    scene: 'pc',
+    name: 'yang',
+  },
+  ebParams_pc: {
+    size: 'large',
+    scene: 'pc',
+    name: 'kevin',
+    group: true,
+  },
+};
 console.log(new Date());
 let res;
 for (let i = 0; i < 100; i++) {
-    res = cascadeExtend({ scope, source, name: 'ebParams' });
+  res = cascadeExtend({ scope, source, name: 'ebParams' });
 }
 console.log(new Date());
 console.log(res);
